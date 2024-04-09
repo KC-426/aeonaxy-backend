6504fee18ac7bedcd9e4d4e105ac7178a59dd683
Kuldeep Chahar <kuldeepchahar426@gmail.com>
2024-04-09 17:45:00 -0300

    update

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,9 @@
-const userSchema = require("../model/user")
+const userSchema = require("../model/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { Resend } = require("resend");
 const fetch = require("node-fetch");
 const cloudinary = require("cloudinary").v2;
-
 
 if (!global.fetch) {
   global.fetch = fetch;
@@ -23,10 +22,9 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-
 const userSignup = async (req, res) => {
   try {
-    const { name,username, email, password } = req.body;
+    const { name, username, email, password } = req.body;
 
     if (!name || !username || !email || !password) {
       return res
@@ -34,7 +32,7 @@ const userSignup = async (req, res) => {
         .json({ message: "Please fill all the required fields!" });
     }
 
-    const existingUser = await userSchema.findOne({email})
+    const existingUser = await userSchema.findOne({ email });
 
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
@@ -48,27 +46,27 @@ const userSignup = async (req, res) => {
 
     const hashedPwd = await bcrypt.hash(password, 12);
 
-    const newUser = new userSchema ({
+    const newUser = new userSchema({
       name,
       username,
       email,
       password: hashedPwd,
     });
 
-    const result = await newUser.save()
+    const result = await newUser.save();
 
     const mail = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: "kuldeepchahar426@gmail.com",
       subject: "Hello World",
-      html: "<p>hello kuldeep, you have successfully signed up! </p>",
+      html: `<p>hello ${name}, you have successfully signed up! </p>`,
     });
 
     console.log(mail, result);
 
     res
       .status(201)
-      .json({ message: "User created successfully!", result, mail });
+      .json({success: true, message: "User created successfully!", result, mail });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
@@ -79,7 +77,7 @@ const userLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const findUser = await userSchema.findOne({email})
+    const findUser = await userSchema.findOne({ email });
 
     if (!findUser) {
       return res
@@ -105,58 +103,42 @@ const userLogin = async (req, res) => {
     console.error(err);
     return res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
 
 const userProfile = async (req, res, next) => {
   const { id } = req.params;
   try {
-    console.log("Request body:", req.body);
-    console.log("Uploaded file:", req.file);
+    const { location } = req.body;
+    const imageFile = req.file;
 
-    const { phone_no, gender } = req.body;
-    const { originalname: image_name, path: image_url } = req.file;
+    console.log(location, req.file)
 
-    const updateUserQuery = `
-      UPDATE users
-      SET phone_no = $1, gender = $2, image_name = $3, image_url = $4
-      WHERE id = $5
-      RETURNING *;
-    `;
+    if (!location) {
+      return res.status(400).json({ message: "Location is required" });
+    }
 
-    const updatedUser = await db.oneOrNone(updateUserQuery, [
-      phone_no,
-      gender,
-      image_name,
-      image_url,
+    const result = await cloudinary.uploader.upload(imageFile.path, {
+      folder: "user_images",
+      public_id: `user_${id}`,
+    });
+
+    const updatedUser = await userSchema.findByIdAndUpdate(
       id,
-    ]);
+      { location, imageFile: result.secure_url },
+      { new: true }
+    );
 
     if (!updatedUser) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    cloudinary.uploader.upload(
-      image_url,
-      { public_id: `user_${id}` },
-      function (error, result) {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log("167 line", result);
-        }
-      }
-    );
+    res.status(200).json({ message: "User profile updated", user: updatedUser });
 
-    res
-      .status(200)
-      .json({ message: "User profile updated", user: updatedUser });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
-
-
 
 
 exports.userSignup = userSignup;
